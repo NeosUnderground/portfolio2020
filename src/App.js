9a7ed1fb540453eb7da1c662e553f9f27551da3f
NeosUnderground <33915360+NeosUnderground@users.noreply.github.com>
2020-10-19 20:45:03 -0700

    update

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ import './App.css';
         <ScrollToTop/>
           <div className="App">
             <Nav/>
+            <HomePage component={HomePage}/>
               <Switch>
                 <HomePage path='/' exact component={HomePage}/>
                 <ServicesPage path='/Services' component={ServicesPage}/>

62fbead28ae7c4602baa4882d37fe2db43634d9f
James Kubitz <jkubitz94@gmail.com>
2024-03-10 20:43:49 -0600

    Updates

---
+++ precache-manifest.6af024b2f39607d6b6a69b4d0099efcb.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30db9941ee4ab6f9bf95e207541fc07d",
+    "url": "/portfolio2020/index.html"
+  },
+  {
+    "revision": "1cc99ab58539dc9b1925",
+    "url": "/portfolio2020/static/css/main.7810cdba.chunk.css"
+  },
+  {
+    "revision": "1e703a67c8f2f12463de",
+    "url": "/portfolio2020/static/js/2.3e609f27.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/portfolio2020/static/js/2.3e609f27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cc99ab58539dc9b1925",
+    "url": "/portfolio2020/static/js/main.0e54a691.chunk.js"
+  },
+  {
+    "revision": "b0796beb7aae7099b0d6",
+    "url": "/portfolio2020/static/js/runtime-main.53cce16e.js"
+  },
+  {
+    "revision": "f1d5b91691ac0b10c22a328af369d467",
+    "url": "/portfolio2020/static/media/Angular.f1d5b916.png"
+  },
+  {
+    "revision": "fbc3a3ea916d90629e3d9afa32b9671a",
+    "url": "/portfolio2020/static/media/CSS.fbc3a3ea.png"
+  },
+  {
+    "revision": "5a67cdab63a88c6401d5e1e0fe0c9ee3",
+    "url": "/portfolio2020/static/media/ClickToShuffle.5a67cdab.png"
+  },
+  {
+    "revision": "c8e660ea1885698e9e0bc5af1205727b",
+    "url": "/portfolio2020/static/media/ContactPageHeader.c8e660ea.png"
+  },
+  {
+    "revision": "590cbd8cd6a79fe0cd428301f16567ed",
+    "url": "/portfolio2020/static/media/Csharp.590cbd8c.jpg"
+  },
+  {
+    "revision": "e7256fcc694d1fece0727bca2f4051e8",
+    "url": "/portfolio2020/static/media/ExpressJs.e7256fcc.png"
+  },
+  {
+    "revision": "dae7f44af6bf5c3f4b6eeb4d65eb2bde",
+    "url": "/portfolio2020/static/media/Firebase.dae7f44a.png"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/portfolio2020/static/media/GitHub.89c2c067.png"
+  },
+  {
+    "revision": "223a8411777b91f509a978a811ae0e78",
+    "url": "/portfolio2020/static/media/Gmail.223a8411.png"
+  },
+  {
+    "revision": "2674fe60f45aca5428ca6a74b8c6f01a",
+    "url": "/portfolio2020/static/media/HTMLImg.2674fe60.png"
+  },
+  {
+    "revision": "708645fca23a3ec7ef9d74e39e23a22b",
+    "url": "/portfolio2020/static/media/HomePageHeader.708645fc.png"
+  },
+  {
+    "revision": "a6b96eb558fab22a437e1371d645610b",
+    "url": "/portfolio2020/static/media/Illustrator.a6b96eb5.png"
+  },
+  {
+    "revision": "6d64efe298c84ab1de4d76e0c69cd4d4",
+    "url": "/portfolio2020/static/media/James Kubitz_Logo.6d64efe2.png"
+  },
+  {
+    "revision": "b54fc1c9e4f229859a1b00205f5d14c0",
+    "url": "/portfolio2020/static/media/Javascript.b54fc1c9.png"
+  },
+  {
+    "revision": "f5df9167c5ce2c5ad2c041d1f6c85006",
+    "url": "/portfolio2020/static/media/LinkedIn.f5df9167.png"
+  },
+  {
+    "revision": "38e1a8132ecb1b1d38800e8c95affc5f",
+    "url": "/portfolio2020/static/media/Linkedin.38e1a813.jpg"
+  },
+  {
+    "revision": "3d121f317dbe68545745460f6c029011",
+    "url": "/portfolio2020/static/media/MongoDB.3d121f31.png"
+  },
+  {
+    "revision": "1d4db9ea7c0bacdd98408a1677eb2db3",
+    "url": "/portfolio2020/static/media/NodeImg.1d4db9ea.png"
+  },
+  {
+    "revision": "a7d5c3282d1f002ed0cc25e47c1afdff",
+    "url": "/portfolio2020/static/media/Photoshop.a7d5c328.png"
+  },
+  {
+    "revision": "8e4e44c5eb6bc0cf86fd70a212051329",
+    "url": "/portfolio2020/static/media/Postgress.8e4e44c5.png"
+  },
+  {
+    "revision": "dd0d3b5a9e848e61032279d2d73d4e6f",
+    "url": "/portfolio2020/static/media/Postman.dd0d3b5a.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/portfolio2020/static/media/Redux.59c46753.png"
+  },
+  {
+    "revision": "5da14f0380b2c34c9b18d577a6f1818e",
+    "url": "/portfolio2020/static/media/ResumePageHeader.5da14f03.png"
+  },
+  {
+    "revision": "ae602c0c29e8119c9f2ea49ebbc9a4cb",
+    "url": "/portfolio2020/static/media/ServicePageHeader.ae602c0c.png"
+  },
+  {
+    "revision": "225fbbe09689a4a3219ae1e00fe2280b",
+    "url": "/portfolio2020/static/media/VSCode.225fbbe0.png"
+  },
+  {
+    "revision": "c59653b6c7e9d47d0afe35d47c2d4d43",
+    "url": "/portfolio2020/static/media/WindowsIIS.c59653b6.png"
+  },
+  {
+    "revision": "584d9f322f08684db8ff2217731e474f",
+    "url": "/portfolio2020/static/media/bootstrap.584d9f32.png"
+  },
+  {
+    "revision": "2c4a1b2ddaa13a459a8c3c09e2eff7df",
+    "url": "/portfolio2020/static/media/computerHands.2c4a1b2d.jpg"
+  },
+  {
+    "revision": "3a170bb53963fe14acad39dc02124765",
+    "url": "/portfolio2020/static/media/html.3a170bb5.png"
+  },
+  {
+    "revision": "6338e3b1fefffb51443e613edd2a6b19",
+    "url": "/portfolio2020/static/media/next_logo.6338e3b1.png"
+  },
+  {
+    "revision": "0c90a8779860f4250a995c2a2584d57a",
+    "url": "/portfolio2020/static/media/programming.0c90a877.jpg"
+  },
+  {
+    "revision": "04557c1b0b790482dafb1bf4e082b69e",
+    "url": "/portfolio2020/static/media/reactImg.04557c1b.png"
+  },
+  {
+    "revision": "f4539796fc4d1f56394c0eab3ce77e61",
+    "url": "/portfolio2020/static/media/resumeImg.f4539796.PNG"
+  },
+  {
+    "revision": "24f3a0d79bace84fbc820ed947c6dee3",
+    "url": "/portfolio2020/static/media/whiteDesk.24f3a0d7.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4ac3f697571330f727dc3da58056de7b.js"
+  "/portfolio2020/precache-manifest.6af024b2f39607d6b6a69b4d0099efcb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
